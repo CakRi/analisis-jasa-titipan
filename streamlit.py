fd6432ba7877781d3d26e0db6149d31adb8bab8e
amerycano <merysitumorang.business@gmail.com>
2024-07-01 06:12:20 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -32,6 +32,11 @@ df = load_data('./data/cn1.csv')
 df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
 df = df[df['HS_CODE'].replace('', np.nan).notna()]
 
+# Function to initialize models
+def initialize_models():
+    global model_ident, vectorizer_ident, model_name, vectorizer_name, model_address, vectorizer_address, model_uraian, vectorizer_uraian, sentence_model
+    model_ident, vectorizer_ident, model_name, vectorizer_name, model_address, vectorizer_address, model_uraian, vectorizer_uraian = create_index(df)
+    sentence_model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
 # # Function
 # ## Mencari Kemiripan Importir
 # model_ident, vectorizer_ident, model_name, vectorizer_name, model_address, vectorizer_address, model_uraian, vectorizer_uraian = create_index(df)
@@ -50,39 +55,30 @@ df = df[df['HS_CODE'].replace('', np.nan).notna()]
 
 # Predict button in sidebar
 if st.sidebar.button('Predict'):
-    model_ident, vectorizer_ident, model_name, vectorizer_name, model_address, vectorizer_address, model_uraian, vectorizer_uraian = create_index(df)
-    sentence_model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+    initialize_models()
     try:
-        # Call the find_similar function
         similar_id = find_similar(no_ident, nm_penerima, al_penerima, df, model_ident, vectorizer_ident, model_name, vectorizer_name, model_address, vectorizer_address)
-
-        # Filter the DataFrame
         filtered_df = similar_id[similar_id['Similarity (%)'] > 60].head(10)
-
-        # Display the filtered DataFrame
-        st.markdown(f'### Filtered Similarity Results:')
+        st.markdown('### Filtered Similarity Results:')
         st.write(filtered_df)
     except Exception as e:
         st.markdown('### An error occurred during model prediction')
         st.write(str(e))
-
        
 # Tabs for additional functionalities
 tabs = st.sidebar.radio("Choose an action:", ["Price Range", "HSCode Search"])
 
 if tabs == "Price Range":
     st.subheader("Price Range by Description")
-    uraian_barang = st.text_input("Uraian Barang")
+    uraian_barang = st.text_input("Uraian Barang", key='uraian_barang_input')
 
     if st.button('Predict Price Range'):
-        model_uraian, vectorizer_uraian 
         try:
             if model_uraian is None or vectorizer_uraian is None:
-                st.write("Model and vectorizer for description not initialized.")
-            else:
-                range_harga = get_range(uraian_barang, df, model_uraian_pred, vectorizer_uraian_pred, sentence_model)
-                st.write(f'Harga kisaran min: {range_harga[0]}')
-                st.write(f'Harga kisaran max: {range_harga[1]}')
+                initialize_models()
+            range_harga = get_range(uraian_barang, df, model_uraian, vectorizer_uraian, sentence_model)
+            st.write(f'Harga kisaran min: {range_harga[0]}')
+            st.write(f'Harga kisaran max: {range_harga[1]}')
         except Exception as e:
             st.write(f'Error predicting price range: {str(e)}')
 
